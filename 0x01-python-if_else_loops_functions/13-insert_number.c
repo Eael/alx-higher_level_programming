4f2fa77c0c72a82e5d0eb93f9a29983aa452a3c4
Kwame <1kwameearl@gmail.com>
2023-11-02 04:05:06 -0700

    Linked lists correction 2

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,7 +28,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 
-	while(current && current->next && current->n < number)
+	while(current && current->next && current->next->n < number)
 		current = current->next;
 
 	new->next = current->next;
