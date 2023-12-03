90338edddc7cec26b1457679a8c2ea7e4e1f7605
Kwame <1kwameearl@gmail.com>
2023-12-03 10:35:36 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,19 +33,19 @@ class Rectangle:
             value (int): value for width
     """
     def width(self, value):
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @heightsetter
-     """Sets height of the rectangle
+    """Sets height of the rectangle
         Args:
             value (int): value for width
     """
     def height(self, value):
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
