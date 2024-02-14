4270f55a9d7436082622e81f254d958d5e0536f6
Eael <1kwameearl@gmail.com>
2024-02-14 00:56:28 -0800

    Semistandard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let message = "C is fun"
-console.log(message)
-message = "Python is cool"
-console.log(message)
-message = "Javascript is amazing"
-console.log(message)
+let message = 'C is fun';
+console.log(message);
+message = 'Python is cool';
+console.log(message);
+message = 'Javascript is amazing';
+console.log(message);
