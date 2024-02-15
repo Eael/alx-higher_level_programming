b7a9faf5b678abfee81eaebe3234887fec7dc424
Eael <1kwameearl@gmail.com>
2024-02-15 08:00:37 -0800

    12

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const message = ['C is fun','Python is cool','JavaScript is amazing'];
-for (i of message) {
-	console.log(i);
+const message = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i of message) {
+  console.log(i);
 }
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * A script that prints the addition of two numbers from argument
+ * define function add(a,b)
+ */
+function add(a,b) {
+  const c = a + b;
+  console.log(c);
+}
+add(Number(process.argv[2]), Number(process.argv[3]));
