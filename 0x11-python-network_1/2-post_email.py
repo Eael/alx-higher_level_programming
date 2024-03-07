0f434efbaaf5d2cac77db1ebf834a599d2f41a8b
Kwame <1kwameearl@gmail.com>
2024-03-07 08:21:58 -0800

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode("ascii")
-    
+
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
