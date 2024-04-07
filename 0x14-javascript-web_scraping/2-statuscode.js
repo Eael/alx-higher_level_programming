760da710ac3bc6a84712810acaf60447a5c8ed56
JS Box <1kwameearl@gmail.com>
2024-04-07 11:49:30 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
-  console.log(err || `code:  ${response.statusCode}`);
+  console.log(err || `code: ${response.statusCode}`);
 });
