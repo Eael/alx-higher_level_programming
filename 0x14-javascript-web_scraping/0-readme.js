b62c68c4642a16c1543ee4fcf6ccdc71a13bb9c3
JS Box <1kwameearl@gmail.com>
2024-04-07 11:43:20 -0700

    semistandared fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@
 const filepath = process.argv[2];
 const fs = require('fs');
 fs.readFile(filepath, 'utf-8', function (error, content) {
-	console.log(error || content);
+  console.log(error || content);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 const id = process.argv[2];
-const endpoint = 'https://swapi-api.alx-tools.com/api/films/'
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/';
 
 request({ url: endpoint + id, method: 'GET' }, function (err, response, body) {
-	console.log(err || body && JSON.parse(body).title);
+  console.log(err || (body && JSON.parse(body).title));
 });
